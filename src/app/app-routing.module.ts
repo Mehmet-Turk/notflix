14ee668bab7695391639c9f3ddf875b3bedf9d56
METURK <turk.mehmet0627@gmail.com>
2023-08-07 15:31:23 +0200

    first push

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from 'src/app/login/login.component';
+import { MainComponent } from 'src/app/main/main.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{path: "", component: MainComponent},
+{path:"login", component:LoginComponent}
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
