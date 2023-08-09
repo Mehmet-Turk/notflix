6825c8217ad3420099b5908675d8ab1b17afcb94
METURK <turk.mehmet0627@gmail.com>
2023-08-09 16:07:26 +0200

    login

---
+++ src/app/sci-fi/sci-fi.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sci-fi',
+  templateUrl: './sci-fi.component.html',
+  styleUrls: ['./sci-fi.component.css']
+})
+export class SciFiComponent {
+
+}
